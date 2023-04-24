08417ce00ec968cf3a5b3ca105f6fa1a5da9e690
jaliss0n <jalisson.santos38@gmail.com>
2023-04-24 20:06:24 -0300

    Barber Home customization buttons added

--- src/pages/barbershop/index.tsx
+++ src/pages/barbershop/index.tsx
@@ -3,17 +3,123 @@ import cover from "../../../images/cover.jpg";
 import profileBarberShop from "../../../images/profile_barbershop.jpg";
 import NewCard from "@/components/newCard";
 import Rodape from "@/components/rodape";
+import { useState } from "react";
+import { Button, IconButton } from "@mui/material";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 
 export default function BarberShop() {
+  const [imgCover, setImgCover] = useState<File | null>(null);
+  const [imgProfileBarber, setImgProfileBarber] = useState<File | null>(null);
+
   return (
     <div className={styles.cardBody}>
       <div className={styles.cardLimit}>
         <div className={styles.cover}>
-          <img src={cover.src} className={styles.coverBarberShop} />
-          <img
-            src={profileBarberShop.src}
-            className={styles.profileBarberShop}
-          />
+          {imgCover ? (
+            <>
+              <IconButton
+                aria-label="upload picture"
+                component="label"
+                sx={{
+                  position: "absolute",
+                  left: "0%",
+                  top: "6%",
+                }}
+              >
+                <input
+                  hidden
+                  accept="image/*"
+                  name="imgCover"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files != null) {
+                      setImgCover(e.target.files[0]);
+                    }
+                  }}
+                />
+                <div className={styles.buttonAlterar}>
+                  <AddAPhotoIcon sx={{ marginRight: "15px", color: "white" }} />
+                  <p>ALTERAR</p>
+                </div>
+              </IconButton>
+              <img
+                src={URL.createObjectURL(imgCover)}
+                className={styles.coverBarberShop}
+              />
+            </>
+          ) : (
+            <div className={styles.coverBarberShopDefault}>
+              <IconButton aria-label="upload picture" component="label">
+                <input
+                  hidden
+                  accept="image/*"
+                  name="imgCover"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files != null) {
+                      setImgCover(e.target.files[0]);
+                    }
+                  }}
+                />
+                <AddAPhotoIcon sx={{ fontSize: "5rem", color: "white" }} />
+              </IconButton>
+            </div>
+          )}
+
+          {imgProfileBarber ? (
+            <div className={styles.imageProfileBarberBody}>
+              <IconButton aria-label="upload picture" component="label">
+                <input
+                  hidden
+                  accept="image/*"
+                  name="imgProfileBarber"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files != null) {
+                      setImgProfileBarber(e.target.files[0]);
+                    }
+                  }}
+                />
+                <div className={styles.buttonAlterarProfile}>
+                  <AddAPhotoIcon sx={{ marginRight: "15px", color: "white" }} />
+                  <p>ALTERAR</p>
+                </div>
+              </IconButton>
+
+              <img
+                src={URL.createObjectURL(imgProfileBarber)}
+                className={styles.profileBarberShop}
+              />
+            </div>
+          ) : (
+            <div className={styles.profileBarberShopDefault}>
+              <IconButton aria-label="upload picture" component="label">
+                <input
+                  hidden
+                  accept="image/*"
+                  name="imgProfileBarber"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files != null) {
+                      setImgProfileBarber(e.target.files[0]);
+                    }
+                  }}
+                />
+                <AddAPhotoIcon
+                  sx={{
+                    fontSize: "8rem",
+                    color: "white",
+                    backgroundColor: "#3c73d2",
+                    padding: "10%",
+                    borderRadius: "40px",
+                    "&:hover": {
+                      backgroundColor: "#2854a2",
+                    },
+                  }}
+                />
+              </IconButton>
+            </div>
+          )}
         </div>
 
         <div className={styles.titleBarberShop}>
