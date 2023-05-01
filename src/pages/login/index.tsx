948c6115d1869b5a5735ff413a0ad9a26de0c6ea
guilh <guiilherme-reis@hotmail.com>
2023-05-01 19:52:42 -0300

    Tela Login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/login/login.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.bodyLogin}>
+      <h1>Login</h1>
+    </div>
+  );
+}
